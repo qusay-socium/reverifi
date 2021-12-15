131b4ecbb00d099be0c1e617f46a7f4ccbae89ac
Anas AL-ramahi <anas@anas-pc.scoiumtech.local>
2021-12-15 17:48:12 +0200

    fixed some rem units, and removed unwanted width properties

--- src/components/ReverifiPlus/reverifi-plus.styles.js
+++ src/components/ReverifiPlus/reverifi-plus.styles.js
@@ -7,7 +7,7 @@ export const ReverifiPlusContainer = styled.div`
   flex-direction: column;
   gap: 2.25rem;
   margin: 0 auto 0 auto;
-  padding: 2.25rem 1.375rem;
+  padding: 2.25rem 1.37rem;
 
   ${mq.tablet`
     flex-direction: row;
@@ -16,13 +16,12 @@ export const ReverifiPlusContainer = styled.div`
 `;
 
 export const PlusUpperSection = styled.div`
-  max-width: 34.125rem;
-  width: 100%;
+  max-width: 34.12rem;
 `;
 
 export const Header = styled.h3`
   font-size: 1.375rem;
-  margin: 0 0 1.125rem 0;
+  margin: 0 0 1.1rem 0;
 
   & span {
     color: ${colors.atlantis};
@@ -40,7 +39,7 @@ export const Paragraph = styled.p`
   opacity: 0.5;
 
   ${mq.tablet`
-    font-size: 1.125rem;
+    font-size: 1.1rem;
   `}
 `;
 
@@ -64,7 +63,6 @@ export const PlusBottomSection = styled.div`
   display: grid;
   gap: 2.5rem;
   grid-template-columns: repeat(2, 1fr);
-  max-width: 34.3rem;
 
   ${mq.mobile`
     grid-template-columns: repeat(3, 1fr);
@@ -80,11 +78,6 @@ export const Item = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-  width: 100%;
-
-  ${mq.tablet`
-    max-width: 10.875rem;
-  `}
 `;
 
 export const ItemIcon = styled.div`
@@ -94,11 +87,11 @@ export const ItemIcon = styled.div`
 `;
 
 export const ItemHeader = styled.h4`
-  font-size: 0.875rem;
+  font-size: 0.87rem;
   font-weight: 400;
   margin: 0.63rem 0 0 0;
 
   ${mq.tablet`
-    font-size: 1.125rem;
+    font-size: 1.1rem;
   `}
 `;
