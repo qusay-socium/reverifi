a65b36c2f52037d408955e8f26b1641ff626a88f
IbrahimAdereni-socuimtech <ibrahim.aldereni@optimumpartners.co>
2021-12-22 11:35:43 +0200

    create login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,41 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { yupResolver } from '@hookform/resolvers/yup';
+import { ReactComponent as AppleIcon } from 'assets/icons/apple.svg';
+import { ReactComponent as FacebookIcon } from 'assets/icons/facebook.svg';
+import { ReactComponent as GoogleIcon } from 'assets/icons/google.svg';
+import { ReactComponent as MainImg } from 'assets/icons/login-main.svg';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import {
+  CheckboxInputContainer,
+  CheckboxInputField,
+  ErrorMessage,
+  FormContainer,
+  ImageContainer,
+  InfoContainer,
+  InputField,
+  InputLabel,
+  LoginContainer,
+  SocialButton,
+  SocialLinksContainer,
+  SocialLinksText,
+  SubmitButton,
+  Title,
+} from './login.styles';
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    name: yup.string().required(),
+    password: yup
+      .number()
+      .positive()
+      .integer()
+      .required()
+      .typeError('password must be a number'),
+  })
+  .required();
 
 /**
  * Login page component.
@@ -6,10 +43,72 @@ import React from 'react';
  * @return {JSX.Element}
  */
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log('form data', data);
+    reset();
+  };
+
   return (
-    <div>
-      <h3>TODO: implement login page.</h3>
-    </div>
+    <LoginContainer>
+      <ImageContainer>
+        <MainImg />
+      </ImageContainer>
+      <InfoContainer>
+        <Title>Log In</Title>
+
+        <FormContainer onSubmit={handleSubmit(onSubmit)}>
+          <InputLabel htmlFor="email">E-mail</InputLabel>
+          <InputField
+            type="email"
+            {...register('email')}
+            id="email"
+            placeholder="eg: Jhon@domain.com"
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <InputField
+            type="password"
+            {...register('password')}
+            id="password"
+            placeholder="password"
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+          <CheckboxInputContainer>
+            <CheckboxInputField type="checkbox" {...register('rememberMe')} />
+            <InputLabel>Remember me</InputLabel>
+          </CheckboxInputContainer>
+
+          <SubmitButton type="submit">Log In</SubmitButton>
+        </FormContainer>
+
+        <SocialLinksContainer>
+          <SocialLinksText>Or</SocialLinksText>
+          <SocialButton blue>
+            <FacebookIcon />
+            Continue with Facebook
+          </SocialButton>
+          <SocialButton dark>
+            <AppleIcon />
+            Continue with Apple
+          </SocialButton>
+          <SocialButton light>
+            <GoogleIcon />
+            Continue with Google
+          </SocialButton>
+        </SocialLinksContainer>
+      </InfoContainer>
+    </LoginContainer>
   );
 }
 
---
+++ src/pages/Login/login.styles.js
@@ -0,0 +1,151 @@
+import checkbox from 'assets/icons/chackbox.svg';
+import styled from 'styled-components';
+import colors from 'styles/colors';
+import mq from 'styles/media-query';
+
+export const LoginContainer = styled.div`
+  display: flex;
+  gap: 180px;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+export const ImageContainer = styled.div`
+  align-items: center;
+  background-color: ${colors.lightgrey2};
+  display: none;
+  flex: 1;
+  justify-content: center;
+
+  ${mq.desktopWide`
+    display: flex;
+  `}
+`;
+
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 75px 0 75px 180px;
+
+  ${mq.desktopWide`
+    padding: 75px 0;
+  `}
+`;
+
+export const Title = styled.h3`
+  color: ${colors.black};
+  font-size: 1.375rem;
+  font-weight: bold;
+  letter-spacing: -0.0229rem;
+  margin: 0;
+  padding-bottom: 1.25rem;
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding-right: 180px;
+`;
+
+export const InputLabel = styled.label`
+  color: ${colors.grey2};
+  font-size: 0.9375rem;
+  padding-bottom: 0.625rem;
+`;
+
+export const InputField = styled.input`
+  border-radius: 1.1875rem;
+  border: 1px solid ${colors.lightgrey3};
+  padding: 0.625rem;
+
+  ::placeholder {
+    color: ${colors.lightgrey3};
+  }
+
+  &:focus {
+    outline: 1px solid ${colors.lightgreen};
+  }
+`;
+
+export const CheckboxInputContainer = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 0.5rem;
+  margin-bottom: 1.25rem;
+
+  label {
+    padding: 0;
+  }
+`;
+
+export const CheckboxInputField = styled.input`
+  -moz-appearance: none;
+  -o-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  border-radius: 4px;
+  border: 2px solid ${colors.lightgrey3};
+  cursor: pointer;
+  height: 1.125rem;
+  outline: none;
+  width: 1.125rem;
+
+  &:checked {
+    background-repeat: no-repeat;
+    background-size: contain;
+    background: url(${checkbox});
+    border: none;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  background-color: ${colors.lightgreen};
+  border-radius: 1.5rem;
+  border: none;
+  color: ${colors.white};
+  cursor: pointer;
+  font-weight: 600;
+  letter-spacing: -0.0067rem;
+  margin-top: 0.625rem;
+  padding: 0.625rem;
+  text-align: center;
+`;
+
+export const SocialLinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 180px;
+  text-align: center;
+`;
+
+export const SocialLinksText = styled.p`
+  border-top: ${({ borderTop }) =>
+    borderTop && `1px solid ${colors.lightgrey3}`};
+  color: ${colors.grey2};
+  font-size: 0.9375rem;
+  margin: 0;
+  margin-top: ${({ borderTop }) => (borderTop ? '2.1875rem' : '0.9375rem')};
+  padding: ${({ borderTop }) => borderTop && '1.5625rem'};
+
+  a {
+    font-size: 0.9375rem;
+  }
+`;
+
+export const SocialButton = styled(SubmitButton)`
+  align-items: center;
+  background-color: ${colors.white};
+  border: ${({ light }) => light && `1px solid ${colors.lightgrey3}`};
+  color: ${({ light }) => light && colors.black};
+  display: flex;
+  font-weight: normal;
+  gap: 2.9375rem;
+  justify-content: center;
+  ${({ blue }) => blue && `background-color: ${colors.blue}; gap: 2.1875rem;`}
+  ${({ dark }) => dark && `background-color: ${colors.black}; gap: 3.75rem;`};
+`;
+
+export const ErrorMessage = styled.p`
+  color: ${colors.red};
+  font-size: 0.75rem;
+  margin: 0.3125rem 0 0.9375rem 0;
+`;
