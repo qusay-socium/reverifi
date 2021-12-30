0bcb19f00442861fdd263cdbf8456fc30f32836d
Anas AL-ramahi <anas@anas-pc.scoiumtech.local>
2021-12-30 16:58:11 +0200

    removed the components from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,6 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <ContactForm />
-      <ScheduleVisit />
       <Storyboard />
       <ShowModalProvider>
         <FeaturedListing />
