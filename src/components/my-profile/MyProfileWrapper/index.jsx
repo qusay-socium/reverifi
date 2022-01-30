0eb9017a07ce0cb544edb320c3dbe7b625f1a25a
IbrahimAdereni-socuimtech <ibrahim.aldereni@optimumpartners.co>
2022-01-30 16:28:29 +0200

    save work

--- src/components/my-profile/MyProfileWrapper/index.jsx
+++ src/components/my-profile/MyProfileWrapper/index.jsx
@@ -1,4 +1,14 @@
+import avatar from 'assets/images/avatar.svg';
 import React from 'react';
+import {
+  ImageContainer,
+  ProfileContainer,
+  TextContainer,
+  UserDescription,
+  UserImage,
+  UserInfoContainer,
+  UserName,
+} from './my-profile-wrapper.styles';
 
 /**
  * My Profile Wrapper component.
@@ -6,7 +16,22 @@ import React from 'react';
  * @return {JSX.Element}
  */
 function MyProfileWrapper() {
-  return <div />;
+  return (
+    <ProfileContainer>
+      <UserInfoContainer>
+        <ImageContainer>
+          <UserImage src={avatar} alt="user-image" />
+        </ImageContainer>
+
+        <TextContainer>
+          <UserName>Lia Adams</UserName>
+          <UserDescription>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </UserDescription>
+        </TextContainer>
+      </UserInfoContainer>
+    </ProfileContainer>
+  );
 }
 
 export default MyProfileWrapper;
---
+++ src/components/my-profile/MyProfileWrapper/my-profile-wrapper.styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+  margin: 2.2rem 12.5rem 2.2rem 4.5rem;
+`;
+
+export const UserInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.4rem;
+`;
+
+export const ImageContainer = styled.div`
+  position: relative;
+`;
+
+export const UserImage = styled.img`
+  width: 8.75rem;
+  height: 8.75rem;
+`;
+
+export const TextContainer = styled.div`
+  //
+`;
+
+export const UserName = styled.h3`
+  //
+`;
+
+export const UserDescription = styled.p`
+  //
+`;
