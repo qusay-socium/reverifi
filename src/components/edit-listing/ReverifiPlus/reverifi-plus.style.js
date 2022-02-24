b6e47a486604b4f01924c2515bfb9ccd52a0a944
Ibrahim Aljabr <ibrahim.aljabr@optimumpartners.co>
2022-02-24 11:06:34 +0200

    create coming soon for reverifi plus

---
+++ src/components/edit-listing/ReverifiPlus/reverifi-plus.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import colors from 'styles/colors';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const HeaderText = styled.h2`
+  font-size: 2rem;
+  font-weight: 600;
+  margin-top: 3.5rem;
+  display: flex;
+  align-items: center;
+
+  svg {
+    path {
+      fill: ${colors.green};
+    }
+    width: 1rem;
+    height: 1rem;
+    margin-left: 0.3rem;
+  }
+`;
