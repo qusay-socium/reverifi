5adbf24a7d666ef1b822e11f925c457c9a60742c
IbrahimAdereni-socuimtech <ibrahim.aldereni@optimumpartners.co>
2022-03-02 17:49:17 +0300

    add special tags enhancements

--- src/components/agent-details/CardImage/index.jsx
+++ src/components/agent-details/CardImage/index.jsx
@@ -17,12 +17,13 @@ import {
  *
  * @return {JSX.Element}
  */
-function CardImage({ listingType, address, images }) {
+function CardImage({ listingType, address, images, tags }) {
   return (
     <CardImageContainer>
       <Image src={images?.[0] || listingImage} />
       <CornerItems isRight={false} isBottom={false}>
         {listingType && <Tag>{listingType}</Tag>}
+        {tags?.length > 0 && tags?.map((tag) => <Tag>{tag}</Tag>)}
       </CornerItems>
 
       {images?.length > 0 && (
@@ -46,10 +47,15 @@ function CardImage({ listingType, address, images }) {
   );
 }
 
+CardImage.defaultProps = {
+  tags: [],
+};
+
 CardImage.propTypes = {
   address: PropTypes.string.isRequired,
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
   listingType: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default CardImage;
