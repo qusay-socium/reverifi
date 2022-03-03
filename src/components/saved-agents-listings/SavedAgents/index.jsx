f77dfaa5e8579b8d338a00f22eda5e0b70cb1bac
IbrahimAdereni-socuimtech <ibrahim.aldereni@optimumpartners.co>
2022-03-03 18:18:58 +0300

    fix bugs

--- src/components/saved-agents-listings/SavedAgents/index.jsx
+++ src/components/saved-agents-listings/SavedAgents/index.jsx
@@ -102,7 +102,15 @@ function SavedAgents() {
       {users?.data?.length > 0 ? (
         <>
           <Table
-            headers={['IMAGE', 'NAME', 'ROLE', 'PHONE', 'EMAIL', 'ADDRESS', '']}
+            headers={[
+              'IMAGE',
+              'AGENT NAME',
+              'AGENT TYPE',
+              'PHONE',
+              'EMAIL',
+              'ADDRESS',
+              '',
+            ]}
           >
             {users?.data?.map(
               ({ savedUser: { id, userInfo, roles, name, phone, email } }) => (
