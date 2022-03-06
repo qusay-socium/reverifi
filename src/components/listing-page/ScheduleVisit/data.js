c006c0969d100e27ae79fac720c191f4a70dfd76
Ibrahim Aljabr <ibrahim.aljabr@optimumpartners.co>
2022-03-06 16:40:48 +0300

    fix schedule slider

--- src/components/listing-page/ScheduleVisit/data.js
+++ src/components/listing-page/ScheduleVisit/data.js
@@ -51,41 +51,6 @@ export const mockDates = [
 
 export const breakPoints = [
   {
-    breakpoint: 1370,
-    settings: {
-      slidesToScroll: 6,
-      slidesToShow: 6,
-    },
-  },
-  {
-    breakpoint: 1250,
-    settings: {
-      slidesToScroll: 5,
-      slidesToShow: 5,
-    },
-  },
-  {
-    breakpoint: 1130,
-    settings: {
-      slidesToScroll: 4,
-      slidesToShow: 4,
-    },
-  },
-  {
-    breakpoint: 1030,
-    settings: {
-      slidesToScroll: 3,
-      slidesToShow: 3,
-    },
-  },
-  {
-    breakpoint: 980,
-    settings: {
-      slidesToScroll: 7,
-      slidesToShow: 7,
-    },
-  },
-  {
     breakpoint: 740,
     settings: {
       slidesToScroll: 6,
