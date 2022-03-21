f896a875199eb252931fedee4f0b224342a28a6c
Ibrahim Aljabr <ibrahim.aljabr@optimumpartners.co>
2022-03-21 16:23:32 +0300

    fix email text

--- src/components/transaction/DataCard/data-card.styles.js
+++ src/components/transaction/DataCard/data-card.styles.js
@@ -6,28 +6,28 @@ export const Card = styled.div`
   border-radius: 0.5rem;
   box-shadow: 0 0.06rem 0.3rem ${colors.midGray};
   margin: 0 0 0.6rem;
-  padding: 0.8125rem 1rem;
-  min-width: 25.6875rem;
-  max-height: 8.125rem;
+  padding: 0.8rem 1rem;
+  min-width: 26rem;
+  max-height: 8.5rem;
 `;
 
 export const PrimaryText = styled.div`
   color: ${colors.mineShaft};
-  font-size: 1.125rem;
+  font-size: 1.12rem;
   font-weight: bold;
-  margin-bottom: 0.375rem;
+  margin-bottom: 0.3rem;
 `;
 
 export const SecondaryText = styled.div`
   color: ${colors.osloGray};
   font-size: 0.875rem;
-  margin: 0.1875rem 0 0 0;
+  margin: 0.18rem 0 0 0;
 `;
 
 export const Services = styled.div`
   align-items: center;
   flex-direction: row;
   display: flex;
-  margin: 0.9375rem 0rem 0.8125rem 0rem;
-  gap: 0.9375rem;
+  margin: 0.9375rem 0rem 0.8rem 0rem;
+  gap: 0.93rem;
 `;
